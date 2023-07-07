48d643ac1fac06e7fe6cb9d35a51b87ad5a912fe
govindgurjar <mr.govindbod@gmail.com>
2023-07-07 23:11:57 +0530

    some changes

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,6 +12,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: SafeArea(
         child: Scaffold(
           bottomNavigationBar: BottomNavigationBar(
